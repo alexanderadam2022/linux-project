1e00f3dafc2aa7cfa00d7373fbd7cee7c6222dc2
adam <alexanderadam2022@gmail.com>
2022-12-14 09:50:28 +0300

    added

--- First_Part/script.sh
+++ First_Part/script.sh
@@ -1,1 +1,4 @@
 cat event_history.csv | grep serdar | grep -i Terminateinstance | grep -Eo "i-[a-zA-Z0-9]{17}" > /tmp/result.txt
+cat event_history.csv | grep "serdar" | grep -i "Terminate" | grep -Eo  "i-[a-zA-Z0-9]{17}" | sort | uniq > result.txt
+cat event_history.csv | grep "serdar" | grep -i "Terminate" | grep -Eo  "i-.{17}" | sort | uniq > result.txt
+cat event_history.csv | grep "serdar" | grep -i "Terminate" | grep -o  "i-.\{17\}"| sort | uniq > result.txt
